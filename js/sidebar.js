567f95a2b0f8efd9ac441acadfb9550d14d0be8a
Alan Quintana <alanqff@gmail.com>
2024-06-02 16:13:15 -0600

    sidebar
    cambios en la visualización de el mismo.

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,6 +3,21 @@ const sidebar = document.getElementById('sidebar');
 const toggleButton = document.getElementById('ocultar');
 const mostrarButton = document.getElementById('mostrar');
 const contenido = document.getElementById('content');
+///  variables para ocultar el sidebar cuando se cambie a otra pagina:
+
+const sidebar_ocultoxdefecto = document.getElementById('sidebar');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.getElementById('sidebar');
+  const contenido = document.getElementById('content');
+
+  sidebar.classList.add('d-none');
+  contenido.classList.remove('col-md-10', 'col-sm-9');
+  contenido.classList.add('col-md-15', 'col-sm-13');
+});
+
+ 
+
 
 // Agregar evento de clic al botón "☰/ocultar"             
 toggleButton.addEventListener('click', () => {
@@ -22,4 +37,5 @@ mostrarButton.addEventListener('click', () => {
   mostrarButton.style.display = 'none';
   // Modificar las clases de contenid5o al mostrar la barra lateral
   contenido.classList.remove('col-md-15', 'col-sm-9');
-  contenido.classList.add('col-md-10', 'col-sm-9');});
+  contenido.classList.add('col-md-10', 'col-sm-9');
+});
